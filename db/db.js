50312735a10d0cdc64a4d9933741072959192647
Alisha092 <ali1382.emp@gmail.com>
2024-03-04 19:44:14 +0330

    setup DB

---
+++ db/db.js
@@ -0,0 +1,15 @@
+// db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log('Database connected');
+    } catch (error) {
+        console.error('Connection error:', error.message);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const connectToDB = require('./db/db');
 require('dotenv').config();
+
+connectToDB();
 
 
 app.get('/', (req, res) => {
