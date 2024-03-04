2d2de06d45f7e99e1c8870762642b698492a858e
Alisha092 <ali1382.emp@gmail.com>
2024-03-04 20:06:02 +0330

    making product model

---
+++ db/models/product.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const productSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    stockQuantity: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    saler: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+
+    
+    
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
